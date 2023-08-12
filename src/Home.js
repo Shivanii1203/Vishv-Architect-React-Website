75b6753de8ebdde233be21bc9976cd3a3d8a3c89
Shivani <shivanilokhande1111@gmail.com>
2023-08-12 10:36:40 +0530

    new added

---
+++ src/Home.js
@@ -0,0 +1,89 @@
+
+import React, { useState } from 'react';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  ContactsOutlined,
+  LogoutOutlined ,
+  UserOutlined
+} from '@ant-design/icons';
+import logo from './image/login page logo.png'
+import { Layout, Menu, Button, theme} from 'antd';
+import Projects from './components/Projects';
+import Contacts from './components/Contacts';
+
+const {  Sider } = Layout;
+
+
+const Home = () => {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const[contact,setContact] = useState(false);
+  
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed} style={{backgroundColor:'#202020'}}>
+        <div className=" login-header logo"  >
+             <img src={logo} alt="" style={{   width: '35px'}}/>
+              <h1>VISHV</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined onClick={()=>setContact(false)}/>,
+              label: 'Projects',
+              
+            },
+            {
+              key: '2',
+              icon: <ContactsOutlined onClick={()=>setContact(true)}/>,
+              label: 'Contacts',
+            },
+            {
+              key: '3',
+              icon: <LogoutOutlined />,
+              label: 'Logout',
+            },
+          ]}
+        style={{backgroundColor:'#252525',height:'100vh',outline:'none',border:'none'}}
+        />
+      </Sider>
+      <div style={{width:'100%'}}>
+
+          <div className="Header" style={{ padding: 0, background: colorBgContainer,backgroundColor:'#252525' }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+              color:'white',
+              // position:'absolute',
+              
+            }}
+          />
+
+          </div>
+          <div className="content">
+            {
+              contact?<Contacts/>:<Projects/>
+            }
+            
+          </div>
+      </div>
+      
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ src/components/Contacts.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import {Table } from 'antd'
+
+export default function Contact() {
+
+    const dataSource = [
+    {
+      key: '1',
+      Name: 'Test',
+      Email: 'test2507@gmail.com',
+      Phone: '9876543210',
+      Message: 'Test message',
+      Date:  '07/25/2023' ,
+      Action: 'Delete',
+    },
+                                         
+    {
+     key: '2',
+      Name: 'Test',
+      Email: 'test@gmail.com',
+      Phone: '9876543210',
+      Message: '	Need to do interior design at our new office',
+      Date:  '07/25/2023' ,
+      Action: 'Delete', 
+    },
+
+  ];
+  
+  const columns =
+   [
+    {
+      title: 'Name',
+      dataIndex: 'Name',
+      key: 'Name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'Email',
+      key: 'Email',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'Phone',
+      key: 'Phone',
+    },
+
+    {
+      title: 'Message',
+      dataIndex: 'Message',
+      key: 'Message',
+    },
+
+    {
+      title: 'Date',
+      dataIndex: 'Date',
+      key: 'Date',
+    },
+
+    {
+      title: 'Action',
+      dataIndex: 'Action',
+      key: 'Action',
+    },
+  ];
+
+  return (
+    <div id='contact'
+        style={{
+          margin:'100px',
+          marginRight:'100px',
+          marginTop:'150px'
+        }}
+    >
+      <div>
+        <h1>List of all Contacts</h1>
+      </div>
+       <Table dataSource={dataSource} columns={columns} />
+    </div>
+  )
+}
